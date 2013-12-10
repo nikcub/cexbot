8500348bfbef466f3de1de615f3a8e5cd9468662
Nik Cubrilovic <nikcub@gmail.com>
2013-12-11 04:12:51 +1100

    new layout

--- cexbot/command_utils.py
+++ cexbot/command_utils.py
@@ -3,27 +3,13 @@
 """ cexbot - command_utils.py
 
 Default command line utitlities to run cexbot
-
-@TODO improve the long helps
 """
 
-import os
-import sys
-import logging
-import argparse
-
-import cexbot.config
-import cexbot.tasks
-
-from cexbot.db import DbManager
-from cexbot.cexapi import CexAPI
-from cexbot.updater import check_update
-from cexbot.timer import ReqTimer
-from cexbot.cex import CexMethods
-
+import os, sys, logging
+import cexbot, config, parser, db, cexapi, updater, timer, cex
 
 def main(argv=[]):
-  args = get_parser()
+  args = parser.get_parser()
 
   verbose = 1
   if args.verbose:
@@ -47,37 +33,37 @@ def main(argv=[]):
     return True
 
   # make sure this is always above command parsing
-  cexbot.config.first_run()
+  # print config
+  config.first_run()
 
   if verbose == 3:
     print args
 
   if args.command == 'config':
     if args.list:
-      return cexbot.config.list()
+      return config.list()
     elif args.edit:
-      return cexbot.config.edit_config()
+      return config.edit_config()
     elif args.testauth:
-      return cexbot.config.test_auth()
+      return config.test_auth()
     elif args.name and args.value:
-      v = cexbot.config.set(args.name, args.value)
-      return cexbot.config.cprint(args.name)
+      v = config.set(args.name, args.value)
+      return config.cprint(args.name)
     elif args.name:
-      return cexbot.config.cprint(args.name)
+      return config.cprint(args.name)
     logging.error('Invalid config option')
     return 1
 
   elif args.command == 'update':
-    return check_update()
+    return updater.check_update()
 
   # not implemented
   elif args.command == 'cleardata':
-    return cexbot.config.clear_userdata()
+    return config.clear_userdata()
 
 
-  config = cexbot.config.get_config()
-  ac = CexAPI(config.get('cex', 'username'), config.get('cex', 'apikey'), config.get('cex', 'secret'))
-  dbi = DbManager()
+  ac = cexapi.CexAPI(config.get('cex.username'), config.get('cex.apikey'), config.get('cex.secret'))
+  dbi = db.DbManager()
   cx = CexMethods(ac, dbi)
 
   if args.command == 'balance':
@@ -101,51 +87,18 @@ def main(argv=[]):
 
   elif args.command == 'updatequotes':
     logging.info('Running updatequotes')
-    ticker_timer = ReqTimer(2, cx.update_ticker)
+    ticker_timer = timer.ReqTimer(2, cx.update_ticker)
     ticker_timer.start()
 
   elif args.command == 'buybalance':
     logging.info('Running buybalance')
-    balance_timer = ReqTimer(5, ac.buy_balance)
+    balance_timer = timer.ReqTimer(5, ac.buy_balance)
     balance_timer.start()
 
   # @TODO __import__
   # if args.task in cexbot.tasks:
     # cexbot.tasks[args.task]()
 
-def get_parser():
-  parser = argparse.ArgumentParser(prog='cexbot-cli', description='cexbot')
-
-  parser.add_argument('-v', dest='verbose', action='store_true', help='verbose output')
-  parser.add_argument('-d', dest='debug', action='store_true', help='debug output (Warning: lots of output, for developers)')
-
-  subparsers = parser.add_subparsers(description='available subcommands', dest="command")
-
-  parser_config = subparsers.add_parser('config', help='config options')
-  parser_config.add_argument('--list', dest='list', action='store_true', help='list configuration variables')
-  parser_config.add_argument('--edit', dest='edit', action='store_true', help='edit configuration directly')
-  parser_config.add_argument('--testauth', dest='testauth', action='store_true', help='test authentication credentials')
-  parser_config.add_argument('name', type=str, help='option name', nargs='?')
-  parser_config.add_argument('value', type=str, help='option value', nargs='?')
-
-  parser_update = subparsers.add_parser('update', help='check for updates')
-
-  parser_version = subparsers.add_parser('version', help='show version')
-
-  parser_balance = subparsers.add_parser('balance', help='show balance')
-
-  parser_task = subparsers.add_parser('task', help='modify tasks')
-  # parser_config.add_argument('--list', dest='task_list', action='store_true', help='list current tasks')
-  parser_task.add_argument('name', type=str, help='task name', nargs='?')
-
-  parser_order = subparsers.add_parser('order', help='order')
-  parser_order.add_argument('pair', type=str, default="GHS/BTC", nargs='?', help="pair (default: GHS/BTC)")
-  parser_order.add_argument('type', type=str, help='buy or sell (default: buy)', default="buy")
-  parser_order.add_argument('price', type=str, default='market', help='price (default: market)', nargs='?')
-  parser_order.add_argument('amount', type=str, help='amount')
-
-
-  return parser.parse_args()
 
 def cl_error(msg=""):
   print >> sys.stderr, msg
